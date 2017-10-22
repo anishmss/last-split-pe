6e862c14f73e95f1ed321549eb5ca1fa47fceba1
Naruki Yoshikawa <naruki-yoshikawa@g.ecc.u-tokyo.ac.jp>
2017-10-22 14:03:11 +0900

    Add validate script for last-split-pe

---
+++ scripts/validate.py
@@ -0,0 +1,85 @@
+import sys
+import re
+
+cigFile = open(sys.argv[1])
+samFile = open(sys.argv[2])
+
+samLine = samFile.readline().split('\t')
+samSeqName = samLine[0]
+for cigLine in cigFile:
+    cigLine = cigLine.split('\t')
+    cigSeqName = cigLine[0]
+    cigRefName = cigLine[1]
+    cigCigar = cigLine[4]
+    # if true CIGAR is split, i.e. *M*N*M
+    if re.match(r"^\d+M\d+N\d+M$", cigCigar):
+        trueAlignInfo = []
+        currentPos = int(cigLine[2])
+        for match in re.findall(r"\d+[A-Z]", cigCigar):
+            length = int(re.search(r"\d+", match).group())
+            code = match[-1]
+            strand = cigLine[-2]
+            if code == 'M':
+                for i in range(length):
+                    trueAlignInfo.append((cigRefName, currentPos, strand))
+                    currentPos += 1
+            elif code == 'N':
+                currentPos += length
+        predAlignInfo = [None] * len(cigLine[-1])
+        while True:
+            assert(samSeqName == cigSeqName)
+            samCigar = samLine[5]
+            refName = samLine[2]
+            refPos = int(samLine[3])
+            seqPos = 0
+            strand = '?'
+            for match in re.findall(r"\d+[A-Z]", samCigar):
+                length = int(re.search(r"\d+", match).group())
+                code = match[-1]
+                strand = cigLine[-2]
+                if code == 'M':
+                    for i in range(length):
+                        predAlignInfo[seqPos] = (refName, refPos, strand)
+                        refPos += 1
+                        seqPos += 1
+                elif code == 'N':
+                    refPos += length
+                elif code == 'H':
+                    seqPos += length
+                else:
+                    print('Unsupported CIGAR')
+                    break
+            samLine = samFile.readline().split('\t')
+            samSeqName = samLine[0]
+            if samSeqName != cigSeqName:
+                break
+        idx = 0
+        isCheckLeft = True
+        isMatchLeft = False
+        isMatchRight = False
+        for match in re.findall(r"\d+[A-Z]", cigCigar):
+            length = int(re.search(r"\d+", match).group())
+            code = match[-1]
+            strand = cigLine[-2]
+            if code == 'M':
+                if isCheckLeft:
+                    startRight = length
+                    isCheckLeft = False
+                    for i in range(length):
+                        if trueAlignInfo[i][0] == predAlignInfo[i][0]\
+                           and trueAlignInfo[i][1] == predAlignInfo[i][1]:
+                               isMatchLeft = True
+                else:
+                    startRight -= 1
+                    for i in range(length):
+                        if trueAlignInfo[startRight+i][0] == predAlignInfo[startRight+i][0]\
+                           and trueAlignInfo[startRight+i][1] == predAlignInfo[startRight+i][1]:
+                               isMatchRight = True
+        print(cigSeqName, isMatchLeft, isMatchRight)
+    else:
+        while True:
+            samLine = samFile.readline().split('\t')
+            samSeqName = samLine[0]
+            if samSeqName != cigSeqName:
+                break
+cigFile.close()
