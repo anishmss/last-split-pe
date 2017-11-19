c6b950a6ef6defe5fd7a370328e44e2b80eea4fa
Naruki Yoshikawa <naruki.yoshikawa@gmail.com>
2017-11-19 15:40:26 +0900

    Add validation for crossing junction

--- RNAseq/validate_junction.py
+++ RNAseq/validate_junction.py
@@ -20,6 +20,8 @@ with open(sys.argv[3]) as f:
 
 correct = 0
 incorrect = 0
+crossed = 0
+notcrossed = 0
 
 with open(sys.argv[1]) as f:
     for line in f:
@@ -27,15 +29,24 @@ with open(sys.argv[1]) as f:
         start = int(start)
         end = int(end)
         isCorrect = False
+        isCrossed = False
+        r = -1
+        l = -1
         if readName in trueJunctions:
             for info in trueJunctions[readName]:
                 if refName != info[0]:
                     continue
                 else:
+                    # is crossed?
+                    if start <= info[2] and end >= info[1]:
+                        r = abs(info[1]-start)
+                        l = abs(info[2]-end)
+                        isCrossed = True
+                    # is edge correct?
                     trueIntronStart = info[1] + 1
                     trueIntronEnd   = info[2] - 1
                     intronInfo = (info[0], trueIntronStart, trueIntronEnd)
-                    if  intronInfo in ambiguity:
+                    if intronInfo in ambiguity:
                         prefixAmbiguity, suffixAmbiguity = ambiguity[intronInfo]
                         PIS = start + 1 # predicted intron start-point
                         PIE = end - 1 # predicted intron end-point
@@ -49,5 +60,11 @@ with open(sys.argv[1]) as f:
             correct += 1
         else:
             incorrect += 1
-        print(line, isCorrect)
-print("correct|incorrect = {}|{}".format(correct, incorrect))
+        if isCrossed:
+            crossed += 1
+        else:
+            notcrossed += 1
+        #if isCrossed and not isCorrect:
+        #    print("{},{},{}".format(readName, r, l))
+print("precision: {}% correct|incorrect|total = {}|{}|{}".format(correct/float(correct+incorrect), correct, incorrect, (correct+incorrect)))
+print("crossed precision: {}% crossed|not crossed|total = {}|{}|{}".format(crossed/float(crossed+notcrossed), crossed, notcrossed, (crossed+notcrossed)))
