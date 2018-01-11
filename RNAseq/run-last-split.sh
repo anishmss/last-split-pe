169a13097c010f3fa33d44ae22c0de87da283da5
Anish Shrestha <anish.ms.shrestha@gmail.com>
2018-01-11 17:37:55 +0900

    removed unnecessary files

--- RNAseq/run-last-split.sh
+++
@@ -1,3 +0,0 @@
-echo "***** [$0] start " `date +'%Y/%m/%d %H:%M:%S'` " *****"
-lastal -r6 -q18 -a21 -b9 -d96 -e120 -Q0 -i1 db interleaved_t3r1.fa | last-split -g db -m 0.9 -n > preprocessed_t3r1.maf 
-echo "***** [$0] end " `date +'%Y/%m/%d %H:%M:%S'` " *****"
